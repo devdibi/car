d7f9038984fd32af14e784a2cc254397ee4c9842
seunghyeon <kshv02@gmail.com>
2024-02-14 17:45:35 +0900

    파일 업로드 메모

--- backend/car_project/app/views/carTest_Save.py
+++ backend/car_project/app/views/carTest_Save.py
@@ -5,7 +5,14 @@ from app.models.CarInfo import CarInfo
 from app.models.CrackInfo import CrackInfo
 from app.upload import upload_image
 
-bp = Blueprint('carTest_Save', __name__)
+bp = Blueprint('carTest_Save', __name__, url_prefix='/check')
+
+# route = /create
+# car_info를 먼저 생성
+# 완성이 되지 않으면 => 대시보드가 아닌 체크 페이지로 다시 넘어갈 수 있도록
+
+# route = /list
+# 현재 차량의 검사 현황 리스트를 조회
 
 @bp.route('/save', methods=['POST'])
 def save_car_info():
@@ -22,19 +29,7 @@ def save_car_info():
         car = CarInfo(car_number=car_number)
         db.session.add(car)
 
-    @bp.route('/register', methods=['POST'])
-    def register_car():
-        image_file = request.files.get('image')
-
-        # 이미지를 버킷에 업로드하고 URL을 받아옴
-        image_url = upload_image(image_file, image_file.filename)
-
-        # CrackInfo 객체 생성 및 데이터베이스에 저장
-        crack_info = CrackInfo(car_id=None, section=None, crack=None, image_path=image_url)
-        db.session.add(crack_info)
-        db.session.commit()
-
-        return jsonify({'message': 'Car registered successfully'}), 200
+    # -----------------------------
 
     # 사고 정보 저장
     crack_infos = []
@@ -63,3 +58,24 @@ def save_car_info():
     }
     return jsonify(response)
 
+@bp.route('/register', methods=['POST'])
+    def register_car():
+        image_file = request.files.get('image') # multipart-file로 파일 로드
+
+        # 이미지를 버킷에 업로드하고 URL을 받아옴
+        # 테스트 할때는 파일명으로 사용 => 완성하면서 운영할때는 별도로 파일명, 경로 구성
+        image_url = upload_image(image_file, image_file.filename)
+
+        # model에 전송
+        # 응답
+        # crack_info에 저장
+
+        # CrackInfo 객체 생성 및 데이터베이스에 저장
+        # car_id는 임시값 => car_id는 요청에 포함될 예정
+        # Null 보다는 특정 값이라도 있는게 좋다.
+        crack_info = CrackInfo(car_id= 1,section=-1, crack=-1, image_path=image_url)
+        db.session.add(crack_info)
+        db.session.commit()
+
+
+        return jsonify({'message': 'Car registered successfully'}), 200
